a08a6853886998b0aeb0cd512c2e3cf95a25101f
Rabin <98199146+initialrise@users.noreply.github.com>
2022-01-22 18:44:59 +0545

    Added isFull function to stack

--- stack/stack.c
+++ stack/stack.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#define MAXITEM 30
+struct stack{
+	int dataitems[MAXITEM];
+	int top;
+};
+
+void isFull(struct stack *s){
+	if(s->top==MAXITEM)
+		return 1;
+	else
+		return 0;
+}
 
 int main(){
 	printf("Hello World");
