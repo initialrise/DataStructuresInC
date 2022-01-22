c0dcadbfe7eaedbb706b83b2320a2774139f4e45
Rabin <98199146+initialrise@users.noreply.github.com>
2022-01-22 20:24:52 +0545

    More functions added
    Added functions to create emptystack and push elements into stack

--- stack/stack.c
+++ stack/stack.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAXITEM 30
+#define MAXITEM 3
 
 struct stack{
 	int dataitems[MAXITEM];
@@ -7,14 +7,14 @@ struct stack{
 };
 
 int isFull(struct stack *s){
-	if(s->top==MAXITEM)
+	if(s->top==MAXITEM-1)
 		return 1;
 	else
 		return 0;
 }
 
 int isEmpty(struct stack *s){
-	if(s->==-1)
+	if(s->top==-1)
 		return 1;
 	else
 		return 0;
@@ -22,12 +22,32 @@ int isEmpty(struct stack *s){
 
 void Display(struct stack s)
 {
-	for(int i=top;i>-1;i--)
-		printf("%d ",i);
+	for(int i=s.top;i>-1;i--)
+		printf("%d ",s.dataitems[i]);
 }
 
+struct stack CreateEmptyStack(){
+	struct stack s;
+	s.top=-1;
+	return s;
+}
+
+void push(struct stack *s,int x){
+	if(isFull(s))
+		printf("Stack is full\n");
+	else{
+		s->top++;
+		s->dataitems[s->top] = x;
+	}
+}
 
 int main(){
-	printf("Hello World");
+	printf("Stack Implementation in C\n");
+	struct stack mystack = CreateEmptyStack();
+	push(&mystack,100);
+	push(&mystack,10);
+	push(&mystack,13);
+	push(&mystack,43);
+	Display(mystack);
 	return 0;
 }
