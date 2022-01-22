b6cdc028c6f579d1a013989319122e2104f4f525
Rabin <98199146+initialrise@users.noreply.github.com>
2022-01-22 18:50:18 +0545

    Added isEmpty and Display

--- stack/stack.c
+++ stack/stack.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #define MAXITEM 30
+
 struct stack{
 	int dataitems[MAXITEM];
 	int top;
 };
 
-void isFull(struct stack *s){
+int isFull(struct stack *s){
 	if(s->top==MAXITEM)
 		return 1;
 	else
 		return 0;
 }
+
+int isEmpty(struct stack *s){
+	if(s->==-1)
+		return 1;
+	else
+		return 0;
+}
+
+void Display(struct stack s)
+{
+	for(int i=top;i>-1;i--)
+		printf("%d ",i);
+}
+
 
 int main(){
 	printf("Hello World");
